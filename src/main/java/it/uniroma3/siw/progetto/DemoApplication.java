20d7d5097de5a7674c28ab501af8b4460d8a9513
Panemiele <gabriele.romualdi98@gmail.com>
2020-06-05 20:00:13 +0200

    Ciao Claudione

--- src/main/java/it/uniroma3/siw/progetto/DemoApplication.java
+++ src/main/java/it/uniroma3/siw/progetto/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	//ciao Claudione
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
