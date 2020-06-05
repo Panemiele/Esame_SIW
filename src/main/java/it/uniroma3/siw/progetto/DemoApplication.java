c49ae2f6f0b4451c6bb03d9334875524e9b3e3d7
v0rren <77diana1992@gmail.com>
2020-06-05 20:01:34 +0200

    p

--- src/main/java/it/uniroma3/siw/progetto/DemoApplication.java
+++ src/main/java/it/uniroma3/siw/progetto/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	//ciao Claudione
+	// preda
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
